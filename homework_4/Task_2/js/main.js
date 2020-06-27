de0adf3844d57a3e7df0d7d2eb1b6c3deb6b66e3
Zinaida <zinaida.ivanova.1988@mail.ru>
2020-06-27 12:19:26 +0300

    change

--- homework_4/Task_2/js/main.js
+++ homework_4/Task_2/js/main.js
@@ -4,28 +4,8 @@ let app = new Vue({
     data:{
     news:[],
     title: 'Новости кино.', 
-    flag: true,
-    curentPage: [],
     },
- 
-   methods: {
-       showActivePage: function(e){
-             let page = e.target.getAttribute('data-news')
-             for(let i = 0; i < this.news.length; i++){
-                 if(this.news[i].id == page){
-                     this.curentPage.push(this.news[i]);
-                     
-                 }
-             }
-           this.flag = false;
-        },
-        returnPage: function(){
-            this.curentPage.length = 0;
-            this.flag = true
-            return this.curentPage;
-        } 
 
-    },
 
     created: function() {                
          this.news = window.arrNews;
